feade2de7cca4c6039c973399f9059e174a0a240
mukeshchau407 <chymukesh5@gmail.com>
2025-05-14 15:23:42 +0545

    initial commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { NavLink, Navigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
+import {
+  FaBars,
+  FaSearch,
+  FaShoppingCart,
+  FaSignOutAlt,
+  FaTimes,
+} from "react-icons/fa";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const { user, setUser, setShowUserLogin, navigate } = useAppContext();
@@ -29,55 +36,20 @@ const Navbar = () => {
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
-            className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+            className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500 border-r-1 border-gray-300"
             type="text"
             placeholder="Search products"
           />
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.836 10.615 15 14.695"
-              stroke="#7A7B7D"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              clipRule="evenodd"
-              d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783"
-              stroke="#7A7B7D"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <FaSearch className=" cursor-pointer " />
         </div>
 
         <div
           onClick={() => navigate("/cart")}
           className="relative cursor-pointer"
         >
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0"
-              stroke="#615fff"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <button className="absolute -top-2 -right-3 text-xs text-white bg-orange-500 w-[18px] h-[18px] rounded-full">
-            3
+          <FaShoppingCart />
+          <button className="absolute -top-2 -right-2 text-[10px] text-white text-center bg-orange-500 w-[14px] h-[14px] rounded-full">
+            5
           </button>
         </div>
 
@@ -91,10 +63,10 @@ const Navbar = () => {
         ) : (
           <div className="relative group">
             {/* <img src="" className="w-10" alt="" /> */}
-            <h2 className="w-10 h-10 flex items-center justify-center bg-orange-200 border-2 border-orange-600 rounded-full cursor-pointer">
+            <h2 className="w-9 h-9 flex items-center justify-center bg-orange-200 border text-sm rounded-full cursor-pointer">
               MC
             </h2>
-            <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-orange-200 py-2.5 w-30 rounded-md text-sm z-40">
+            <ul className="hidden group-hover:block absolute top-9 right-0 bg-white shadow border border-orange-200 py-2.5 w-30 rounded-md text-sm z-40">
               <li
                 onClick={() => navigate("my-orders")}
                 className="p-1.5 pl-3 hover:bg-orange-200 cursor-pointer"
@@ -103,9 +75,10 @@ const Navbar = () => {
               </li>
               <li
                 onClick={logout}
-                className="p-1.5 pl-3 hover:bg-orange-200 cursor-pointer"
+                className="p-1.5 pl-3 hover:bg-orange-200 cursor-pointer flex items-center gap-1"
               >
                 logout
+                <FaSignOutAlt />
               </li>
             </ul>
           </div>
@@ -115,28 +88,20 @@ const Navbar = () => {
       <button
         onClick={() => (open ? setOpen(false) : setOpen(true))}
         aria-label="Menu"
-        className="sm:hidden"
+        className="sm:hidden transition-all duration-300"
       >
-        {/* Menu Icon SVG */}
-        <svg
-          width="21"
-          height="15"
-          viewBox="0 0 21 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="21" height="1.5" rx=".75" fill="#426287" />
-          <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
-          <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287" />
-        </svg>
+        {!open ? <FaBars /> : <FaTimes />}
       </button>
 
       {/* Mobile Menu */}
+
       {open && (
         <div
-          className={`${
-            open ? "flex" : "hidden"
-          } absolute top-[60px] left-0 w-full bg-white shadow-md py-6 flex-col items-center gap-2 px-5 text-sm md:hidden`}
+          className={`transition-all duration-300 ease-in-out ${
+            open
+              ? "flex opacity-100 translate-y-0"
+              : "hidden opacity-0 -translate-y-2"
+          } absolute top-[60px] left-0 w-full bg-white shadow-md py-6 flex-col items-center gap-2 px-5 text-sm md:hidden z-500`}
         >
           <NavLink to="/" onClick={() => setOpen(false)}>
             Home
