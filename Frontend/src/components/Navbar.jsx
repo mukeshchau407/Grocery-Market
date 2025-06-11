c2b0d5f7787779e815de1a5299bcdab89ba74fe9
mukeshchau407 <chymukesh5@gmail.com>
2025-06-11 22:09:51 +0545

    initial commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     Navigate("/");
   };
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-18 py-4 border-b border-gray-300 bg-white sticky top-0 z-50 transition-all">
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white sticky top-0 z-50 transition-all">
       <NavLink to="/" onClick={() => setOpen(false)}>
         {/* <img
           className="h-9"
