630d9c76192ce38e700053ed0b0e3cc693ee7a6d
mukeshchau407 <chymukesh5@gmail.com>
2025-06-16 23:59:55 +0545

    initial commit

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
   ];
 
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16 bg-orange-500/10">
       <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
         <div>
           <h2 className="text-black font-bold text-3xl">
