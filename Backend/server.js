2192cf32005f45ed0fad70f4719c49edcef8e432
mukeshchau407 <chymukesh5@gmail.com>
2025-07-09 11:53:41 +0545

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,24 @@
-import http from "http";
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello World!");
-});
+const app = express();
+const port = process.env.PORT || 3200;
 
-const PORT = 3000;
+await connectDB();
 
-server.listen(PORT, () => {
-  console.log(`server is running at https://localhost:${PORT}/`);
+//Allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+//Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, Credential: true }));
+
+app.get("/", (req, res) => res.send("Server is running"));
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
