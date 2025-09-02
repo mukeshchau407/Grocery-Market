26c96a8c62b67de8572667f4a22c3505e956da95
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-09-02 18:27:37 +0545

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
-import userRouter from "./routes/userRoute.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,7 +16,7 @@ const allowedOrigins = ["http://localhost:5173"];
 //Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, Credential: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
