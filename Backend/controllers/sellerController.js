e9fa8da146956f4978417f3408f9252ba738a910
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-09-03 22:03:42 +0545

    initial commit

---
+++ Backend/controllers/sellerController.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+//Login Seller : /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      password == process.env.SELLER_PASSWORD &&
+      email == process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d ",
+      });
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      });
+      return res
+        .status(200)
+        .json({ success: true, message: "Login successful" });
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
