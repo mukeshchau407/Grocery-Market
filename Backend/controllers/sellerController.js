61ccec18cb050b38e7e67f8d15e1034bdbf7c26b
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-09-03 22:09:55 +0545

    initial commit

--- Backend/controllers/sellerController.js
+++ Backend/controllers/sellerController.js
@@ -32,3 +32,30 @@ export const sellerLogin = async (req, res) => {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
+
+// Seller isAuth : /api/seller/isAuth
+
+export const isSellerAuth = async (req, res) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Seller Logout : /api/seller/logout
+
+export const sellerLogout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
