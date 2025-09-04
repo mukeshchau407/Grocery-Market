567124e5239652bc754e2b67037faa965ed55213
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-09-04 20:39:02 +0545

    initial commit

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,5 +1,29 @@
+import Product from "../models/Product";
+
 // Add product : /api/product/add
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req, res) => {
+  try {
+    let productData = json.parse(req.body.productData);
+
+    const images = req.files;
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+
+    await Product.create({ ...productData, images: imagesUrl });
+
+    res.json({ success: true, message: "Product added successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 // Get product list : /api/product/list
 
